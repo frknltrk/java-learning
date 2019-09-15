38f0d00eccded19efd6fa54bcdcba9642fcd81f3
Furkan <furkanunluturkk@gmail.com>
2019-09-15 12:29:16 +0300

    updated

---
+++ program-1.1/MainFrame.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+    private JButton button;
+    private TextArea textArea;
+
+    MainFrame() {
+        setDefaultCloseOperation(3);
+        setSize(800, 600);
+        setResizable(false);
+        setLocationRelativeTo(null);
+
+        setLayout(new BorderLayout());
+        
+        button=new JButton("Click Me!");
+        textArea=new TextArea();
+        add(button,BorderLayout.NORTH);
+        add(textArea,BorderLayout.CENTER);
+
+        button.addActionListener(new ActionListener(){
+        
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.appendText("You've just clicked.\n");
+            }
+        });
+
+        setVisible(true);
+    }
+    
+}
